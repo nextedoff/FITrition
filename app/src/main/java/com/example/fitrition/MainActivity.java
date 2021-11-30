5a32d9c41e8025536fbd1619b50fc42056316015
George Alistair Russell <c0016937@newcastle.ac.uk>
2021-11-30 17:53:39 +0000

    Add button functionality for main activity

--- app/src/main/java/com/example/fitrition/MainActivity.java
+++ app/src/main/java/com/example/fitrition/MainActivity.java
@@ -1,21 +1,36 @@
 package com.example.fitrition;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void toRegister(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        loginButton = findViewById(R.id.main_login_button);
+        registerButton = findViewById(R.id.main_register_button);
+
+        // Listens for login button click
+        loginButton.setOnClickListener(v -> {
+            //TODO: Add login intent
+            System.out.println("LOGIN CLICK");
+        });
+
+        // Listens for register button click
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
